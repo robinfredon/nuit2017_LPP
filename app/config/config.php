7b403999ca40e287276513636b007b48a4176bcc
Racourcix <mrracourcix@gmail.com>
2017-12-07 20:34:32 +0100

    config BDD

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
 define ('DB_HOST',  'localhost');
-define ('DB_NAME',  'tutorinsa');
-define ('DB_USER',  'tutorinsa');
-define ('DB_PASS',  'tut0r1ns4');
+define ('DB_NAME',  'nuit');
+define ('DB_USER',  'nuit');
+define ('DB_PASS',  'nuitLPP');
 
 define ('RNA',  "<div style='display:flex;flex-direction:column;width:50%;'><p>requ&ecirc;te non autoris&eacute;e.</p><img src='https://i.imgflip.com/9ywve.jpg' alt='gandhalf'/> ");
 
